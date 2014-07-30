4c9cbd4126dfed47bccb11de4e08cd23638daaf8
Joey <jcrgtr@gmail.com>
2014-07-29 19:20:22 -0700

    Adjust tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,11 @@
 FactoryGirl.define do
 
 reg = /\A[A-Za-z0-9]+(?:[._-][A-Za-z0-9]+)*{3,17}\z/i
-univ = ["arizona", "uoregon", "asu", "nyu", "usc","arizona", "stanford", "ucla", "harvard", "utexas", "vill", "syr", "dartmouth", "ttu"]
 
   factory :user do |f|
   	f.username {"#{Faker::Internet.user_name(specifier: reg)[0..17]}#{Random.rand(1...99)}"}
     f.fullname {Faker::Name.name}
-    f.email {"#{username}@email.#{univ.sample}.edu"}
+    f.email {"#{username}@email.az.edu"}
     f.password "password"
     f.password_confirmation "password"
   end
